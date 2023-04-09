a925f1ed619a51941ace250205d50f22507cd313
Mahdi Delzendeh <m.delzendeh72@gmail.com>
2023-04-09 10:33:13 +0330

    add base provider

---
+++ src/CSharp/EasyMicroservices.Security/Providers/EncryptionAlgorithmBaseProvider.cs
@@ -0,0 +1,56 @@
+﻿using EasyMicroservices.Security.Interfaces;
+using System.Security.Cryptography;
+
+namespace EasyMicroservices.Security.Providers
+{
+    public abstract class EncryptionAlgorithmBaseProvider : IEncryptionAlgorithm
+    {
+        public virtual ReadOnlySpan<byte> Decrypt(ReadOnlySpan<byte> encryptedData, ReadOnlySpan<byte> key)
+        {
+            ReadOnlySpan<byte> decryptedData;
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key.ToArray();
+                aes.IV = encryptedData.ToArray().Take(aes.BlockSize / 8).ToArray();
+
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream ms = new MemoryStream())
+                using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Write))
+                {
+                    cs.Write(encryptedData.ToArray(), aes.BlockSize / 8, encryptedData.Length - aes.BlockSize / 8);
+                    cs.FlushFinalBlock();
+                    decryptedData = ms.ToArray();
+                }
+            }
+
+            return decryptedData;
+        }
+
+        public virtual ReadOnlySpan<byte> Encrypt(ReadOnlySpan<byte> data, ReadOnlySpan<byte> key)
+        {
+            ReadOnlySpan<byte> encryptedData;
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = key.ToArray();
+                aes.GenerateIV();
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream ms = new MemoryStream())
+                using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                {
+                    cs.Write(data.ToArray(), 0, data.Length);
+                    cs.FlushFinalBlock();
+                    encryptedData = ms.ToArray();
+                }
+            }
+            return encryptedData;
+        }
+
+        public virtual bool IsSymmetricAlgorithm()
+        {
+            return true;
+        }
+    }
+}
---
+++ src/CSharp/EasyMicroservices.Security/Providers/HashAlgorithmBaseProvider.cs
@@ -0,0 +1,18 @@
+﻿using EasyMicroservices.Security.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMicroservices.Security.Providers
+{
+    public abstract class HashAlgorithmBaseProvider : IHashAlgorithm
+    {
+        public virtual Span<byte> ComputeHash(Span<byte> buffer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
