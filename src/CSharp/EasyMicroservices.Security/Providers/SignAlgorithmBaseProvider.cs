b96f8bd90cb2d8fc746d2288a499d98ebc9898e6
Mahdi Delzendeh <m.delzendeh72@gmail.com>
2023-04-09 10:33:00 +0330

    add hashing interface and Signature interface

---
+++ src/CSharp/EasyMicroservices.Security/Providers/SignAlgorithmBaseProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EasyMicroservices.Security.Interfaces;
+
+namespace EasyMicroservices.Security.Providers
+{
+    public abstract class SignAlgorithmBaseProvider : ISignatureAlgorithm
+    {
+        public virtual ReadOnlySpan<byte> SignData(ReadOnlySpan<byte> data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual bool ValidateSignature(string data, string signature)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
